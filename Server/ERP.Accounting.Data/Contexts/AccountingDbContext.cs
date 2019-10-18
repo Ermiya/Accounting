18d789f3bdda85ff79d68a37f9568428995edad1
Farshid <Farshid@FARSHID-PC>
2019-10-18 23:22:23 +0330

    Initial commit

---
+++ Server/ERP.Accounting.Data/Contexts/AccountingDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Accounting.Data.Contexts
+{
+    public class AccountingDbContext : DbContext
+    {
+        public AccountingDbContext() : base("name=Default") { }
+        public AccountingDbContext(string connectionString) : base(DbProviderFactories.GetFactory("System.Data.SqlClient").CreateConnection(), true)
+        {
+            Database.Connection.ConnectionString = connectionString;
+            Configuration.ProxyCreationEnabled = true;
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
